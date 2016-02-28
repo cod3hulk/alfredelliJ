ff4cb3285a8b9dd2de01a56c35bb462bc89242d5
Sebastian Puskeiler <s.puskeiler@mytaxi.com>
2016-02-28 19:38:27 +0100

    * updated script

--- getIntelliJProjects.sh
+++ getIntelliJProjects.sh
@@ -1,7 +1,6 @@
 echo "<items>"
-
 for i in $(find $1 -name $2'*.iml'); do
-    echo "<item uid='' valid='yes'><title>$(basename $i)</title><arg>$(dirname $i)</arg></item>"
+    filename=$(basename "$i")
+    echo "<item uid='' valid='yes'><title>${filename%.*}</title><arg>$(dirname $i)</arg></item>"
 done
-
 echo "</items>"
