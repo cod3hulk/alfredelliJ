04615919a3438669021a6d124634d6c36165dc7f
Sebastian Puskeiler <s.puskeiler@mytaxi.com>
2016-03-04 11:09:04 +0100

    * speed up the script

--- getIntelliJProjects.sh
+++ getIntelliJProjects.sh
@@ -1,5 +1,5 @@
 echo "<items>"
-for i in $(find $1 -name $2'*.iml'); do
+for i in $(find $1 -name $2'*.iml' -maxdepth 2); do
     filename=$(basename "$i")
     echo "<item uid='' valid='yes'><title>${filename%.*}</title><arg>$(dirname $i)</arg></item>"
 done
