e3456d4a2be107075768b9509b3db2b47d19f15c
Tomas Ave <cod3hulk@gmail.com>
2016-04-25 13:18:54 +0200

    Added support for fuzzy seawrch

--- getIntelliJProjects.sh
+++ getIntelliJProjects.sh
@@ -1,5 +1,6 @@
+wild_search=$(echo $2 | sed 's/\(.\)/\1*/g')
 echo "<items>"
-for i in $(find $1 -name $2'*.iml' -maxdepth 2); do
+for i in $(find $1 -name $wild_search'*.iml' -maxdepth 2); do
     filename=$(basename "$i")
     echo "<item uid='' valid='yes'><title>${filename%.*}</title><arg>$(dirname $i)</arg></item>"
 done
